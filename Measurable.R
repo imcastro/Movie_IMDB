32766f968e2ee01d4c9d4d949f382e3f04fcf1c3
imcastro <immcastro4@gmail.com>
2020-10-15 15:19:51 -0400

    Add files via upload

---
+++ Measurable.R
@@ -0,0 +1,26 @@
+View(movie)
+
+mean(movie$popularity)
+#21.4923
+
+median(movie$popularity)
+# 12.92159
+
+var(movie$popularity)
+#1012.299
+
+sd(movie$popularity)
+#31.81665
+
+mean(movie$budget)
+#29045040
+
+median(movie$budget)
+#1.5e+07
+
+var(movie$budget)
+#1.658313e+15
+
+sd(movie$budget)
+#40722391
+
